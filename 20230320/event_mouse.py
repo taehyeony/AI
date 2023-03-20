089b6359ce7f543cbecb4b21af53982eb105c2ca
TaehyeonJeon <taehyun0540@gmail.com>
2023-03-20 13:14:59 +0900

    add color change

--- 20230320/event_mouse.py
+++ 20230320/event_mouse.py
@@ -1,18 +1,24 @@
 import numpy as np
 import cv2
 
+img = np.zeros((200,300,3),np.uint8)
+
 def onMouse(event,x,y,flags,param):
     if event == cv2.EVENT_LBUTTONDOWN:
         print('왼쪽 버튼 누름')
+        img[:] = (100,255,255)
+        cv2.imshow(title1,img)
     elif event == cv2.EVENT_RBUTTONDOWN:
         print('오른쪽 버튼 누름')
+        img[:] = (255,100,255)
+        cv2.imshow(title1,img)
     elif event == cv2.EVENT_LBUTTONUP:
         print('왼쪽 버튼 뗌')
     elif event == cv2.EVENT_RBUTTONUP:
         print('오른쪽 버튼 뗌')
     elif event == cv2.EVENT_RBUTTONDBLCLK:
         print('오른쪽 버튼 더블클릭')
-img = np.full((200,300),255,np.uint8)
+
 
 title1, title2 = "Mouse Event1","Mouse Event2"
 cv2.imshow(title1,img)
