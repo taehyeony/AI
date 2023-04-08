d35c8f09ec446753d333a340427edc88d697bc89
TaehyeonJeon <taehyun0540@gmail.com>
2023-04-08 16:54:26 +0900

    AI

--- 4/05.event_trackbar.py
+++ 4/05.event_trackbar.py
@@ -5,3 +5,21 @@ import cv2
 
 def onChange(value):  # 트랙바 콜백 함수
     global image, title  # 전역 변수 참조
+
+    add_value = value - int(image[0][0])  # 트랙바 값과 영상 화소값 차
+    print("추가 화소값:", add_value)
+    image = image + add_value
+    print(image)
+    cv2.imshow(title, image)
+
+
+image = np.zeros((300, 500), np.uint8)
+
+title = "Trackbar Event"
+cv2.imshow(title, image)
+
+# 트랙바 콜백 함수
+cv2.createTrackbar("Brightness", title, image[0][0], 255, onChange)
+#                   트랙바 이름, 윈도우 이름, 현재값, 최대값, 콜백함수
+cv2.waitKey(0)
+cv2.destroyAllWindows()
---
+++ 4/06.event_mouse_trackbar.py
@@ -0,0 +1,44 @@
+# 마우스 및 트랙바 이벤트 사용
+import numpy as np
+import cv2
+
+
+def onChange(value):  # 트랙바 콜백 함수
+    global image, title  # 전역 변수 참조
+
+    add_value = value - int(image[0][0])  # 트랙바 값과 영상 화소값 차
+    print("추가 화소값:", add_value)
+    image = image + add_value
+    print(image)
+    cv2.imshow(title, image)
+
+
+def onMouse(event, x, y, flags, param):
+    global image, bar_name
+
+    if event == cv2.EVENT_RBUTTONDOWN:
+        if image[0][0] < 246:
+            image = image + 10
+        else:
+            image = np.full((300, 500), 255, np.uint8)
+        # 트랙바 위치 변경 - 마우스로 클릭했을 때 트랙바의 위치 변경
+        cv2.setTrackbarPos(bar_name, title, image[0][0])
+        cv2.imshow(title, image)
+
+    if event == cv2.EVENT_LBUTTONDOWN:
+        if image[0][0] > 9:
+            image = image - 10
+        else:
+            image = np.zeros((300, 500), np.uint8)
+        cv2.setTrackbarPos(bar_name, title, image[0][0])
+        cv2.imshow(title, image)
+
+
+image = np.zeros((300, 500), np.uint8)
+title = 'Trackbar & Mouse Event'
+bar_name = 'Brightness'
+cv2.imshow(title, image)
+cv2.createTrackbar(bar_name, title, image[0][0], 255, onChange)
+cv2.setMouseCallback(title, onMouse)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
