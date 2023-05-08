67ce4c16f88e6d5d7fc18ebe2488c8e05770a4cb
TaehyeonJeon <taehyun0540@gmail.com>
2023-05-08 10:32:28 +0900

    .

--- 5/05.exp_log.py
+++ 5/05.exp_log.py
@@ -2,17 +2,17 @@ import numpy as np
 import cv2
 
 # numpy array 생성 예시
-v1 = np.array([1, 2, 3], np.float32)          # 1차원 리스트로 생성- 행벡터
-v2 = np.array([[1], [2], [3]], np.float32)      # 2차원 리스트로(3행, 1열) - 행벡터
-v3 = np.array([[1, 2, 3]], np.float32)        	# 2차원 리스트로(1행, 3열) - 일반 행렬
+v1 = np.array([1, 2, 3], np.float32)  # 1차원 리스트로 생성- 행벡터
+v2 = np.array([[1], [2], [3]], np.float32)  # 2차원 리스트로(3행, 1열) - 행벡터
+v3 = np.array([[1, 2, 3]], np.float32)  # 2차원 리스트로(1행, 3열) - 일반 행렬
 
 # OpenCV 산술 연산 함수는 numpy array만 가능함
-v1_exp = cv2.exp(v1)                             # 벡터에 대한
-v2_exp = cv2.exp(v2)                             # 행렬에 대한 지수 계산
-v3_exp = cv2.exp(v3)                             # 행렬에 대한 지수 계산
-log = cv2.log(v1)                             # 로그 계산
-sqrt = cv2.sqrt(v2)                            # 제곱근 계산
-pow = cv2.pow(v3, 3)                          # 3의 거듭제곱 계산
+v1_exp = cv2.exp(v1)  # 벡터에 대한
+v2_exp = cv2.exp(v2)  # 행렬에 대한 지수 계산
+v3_exp = cv2.exp(v3)  # 행렬에 대한 지수 계산
+log = cv2.log(v1)  # 로그 계산
+sqrt = cv2.sqrt(v2)  # 제곱근 계산
+pow = cv2.pow(v3, 3)  # 3의 거듭제곱 계산
 
 # 결과 출력
 print("[v1] 형태: %s 원소: %s" % (v1.shape, v1))
